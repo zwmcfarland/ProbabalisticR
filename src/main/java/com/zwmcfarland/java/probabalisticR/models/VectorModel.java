06e497bfbc12c9ea00b69ee74b58d711da388c25
Zachary McFarland <zwmcfarland@gmail.com>
2016-11-12 09:58:06 -0600

    Added model definitions for experimentation.

---
+++ src/main/java/com/zwmcfarland/java/probabalisticR/models/VectorModel.java
@@ -0,0 +1,20 @@
+package com.zwmcfarland.java.probabalisticR.models;
+
+import java.util.List;
+
+import com.zwmcfarland.java.probabalisticR.dto.DataSet;
+
+public class VectorModel implements IRModel {
+
+	@Override
+	public void initializeDataStructures(DataSet s) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public double runQuery(List<String> query) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+}
